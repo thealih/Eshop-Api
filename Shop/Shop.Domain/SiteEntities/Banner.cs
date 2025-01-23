36059c4013983408048383cc0b4f38c7830347ff
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-01-23 14:37:05 +0330

    Complete Domain

---
+++ Shop/Shop.Domain/SiteEntities/Banner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.SiteEntities
+{
+    public class Banner : BaseEntity
+    {
+        public Banner(string link, string imageName, BannerPosition position)
+        {
+            Guard(link, imageName);
+            Link = link;
+            ImageName = imageName;
+            Position = position;
+        }
+
+        public string Link { get; private set; }
+        public string ImageName { get; private set; }
+        public BannerPosition Position { get; private set; }
+
+        public void Edit(string link, string imageName, BannerPosition position)
+        {
+            Guard(link, imageName);
+            Link = link;
+            ImageName = imageName;
+            Position = position;
+        }
+
+        public void Guard(string link, string imageName)
+        {
+            NullOrEmptyDomainDataException.CheckString(link , nameof(link));
+            NullOrEmptyDomainDataException.CheckString(imageName , nameof(imageName));
+        }
+    }
+
+    public enum BannerPosition
+    {
+    زیر_اسلایدر,
+    سمت_راست_اسلایدر
+    }
+}
