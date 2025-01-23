b2a1700206bb16bb9a5a4e5b882c4b53b9e78f09
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-01-23 22:46:47 +0330

    Comment Use Case

---
+++ Shop/Shop.Application/Comments/Create/CreateCommentCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Application;
+
+namespace Shop.Application.Comments.Create
+{
+    public record CreateCommentCommand(string Text, long UserId , long ProductId) : IBaseCommand;
+}
