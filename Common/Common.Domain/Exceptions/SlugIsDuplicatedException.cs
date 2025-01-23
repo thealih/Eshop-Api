59ce3bf1d1a17b3b9bb99bd6073f3abb27ca9387
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-01-23 12:11:17 +0330

    Complete OrderAggregate Task

---
+++ Common/Common.Domain/Exceptions/SlugIsDuplicatedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Exceptions
+{
+    public class SlugIsDuplicatedException : BaseDomainException
+    {
+        public SlugIsDuplicatedException():base("slug تکراری است.")
+        {
+
+        }
+        public SlugIsDuplicatedException(string message) : base(message) { }
+    }
+}
