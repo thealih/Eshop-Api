a91d1926398c5177259a97555befe6cba2af3c87
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-01-30 00:22:23 +0330

    start quray layer

---
+++ Shop/Shop.Query/Categories/DTOs/CategoryDto.cs
@@ -0,0 +1,25 @@
+﻿using Common.Domain.ValueObjects;
+
+namespace Shop.Query.Categories.DTOs;
+
+public class CategoryDto
+{
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public SeoData SeoData { get; set; }
+    public List<ChildCategoryDto> Children { get; set; }
+}
+
+public class ChildCategoryDto
+{
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public SeoData SeoData { get; set; }
+    public long? ParentId { get; set; }
+    public List<ChildCategoryDto> Children { get; set; }
+}
+
+public class SecondaryChildCategoryDto
+{
+
+}
