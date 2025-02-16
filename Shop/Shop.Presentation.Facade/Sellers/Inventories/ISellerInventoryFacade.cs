aace93bc44333d2b9b1c4d1d8389d6cd1d6340af
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-02-16 20:24:04 +0330

    update facade layer

---
+++ Shop/Shop.Presentation.Facade/Sellers/Inventories/ISellerInventoryFacade.cs
@@ -0,0 +1,37 @@
+﻿using Common.Application;
+using MediatR;
+using Shop.Application.Sellers.AddInventory;
+using Shop.Application.Sellers.EditInventory;
+using Shop.Query.Sellers.DTOs;
+
+namespace Shop.Presentation.Facade.Sellers.Inventories;
+
+public interface ISellerInventoryFacade
+{
+    Task<OperationResult> AddInventory(AddSellerInventoryCommand command);
+    Task<OperationResult> EditInventory(EditSellerInventoryCommand command);
+
+
+}
+
+internal class SellerInventoryFacade : ISellerInventoryFacade
+{
+    private readonly IMediator _mediator;
+
+    public SellerInventoryFacade(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<OperationResult> AddInventory(AddSellerInventoryCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    public async Task<OperationResult> EditInventory(EditSellerInventoryCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+ 
+}
